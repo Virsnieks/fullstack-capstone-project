a4f83edb870a3cb2f566e787dd2d3d6affb30ed7
Virsnieks <ivars.getlins@gmail.com>
2025-09-12 11:15:11 +0300

    Login button green

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -7,13 +7,13 @@ export default function Navbar() {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <a className="navbar-brand" href="/">GiftLink</a>
             <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
+                <ul className="navbar-nav ms-auto">
 
                     <li className="nav-item">
                     <a className="nav-link" href="/home.html">Home</a> {/* Link to home.html */}</li>
                     <li className="nav-item"><a className="nav-link" href="/app">Gifts</a> {/* Updated Link */}</li>
                     <li className="nav-item"><Link className="nav-link" to="/app/search">Search</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/app/Login">Login</Link></li>
+                    <li className="nav-item"><Link className="nav-link login-btn" to="/app/Login">Login</Link></li>
                     <li className="nav-item"><Link className="nav-link" to="/app/register">Register</Link></li>                 
                 </ul>
             </div>
