764bd02d8bb5268daaefdc55fff60aa3321cd8b1
Virsnieks <ivars.getlins@gmail.com>
2025-09-12 10:36:51 +0300

    Added Login and Register in frontend

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -6,18 +6,15 @@ export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <a className="navbar-brand" href="/">GiftLink</a>
-            <li className="nav-item">
-    <Link className="nav-link" to="/app/search">Search</Link>
-</li>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
 
                     <li className="nav-item">
-                    <a className="nav-link" href="/home.html">Home</a> {/* Link to home.html */}
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/app">Gifts</a> {/* Updated Link */}
-                    </li>
+                    <a className="nav-link" href="/home.html">Home</a> {/* Link to home.html */}</li>
+                    <li className="nav-item"><a className="nav-link" href="/app">Gifts</a> {/* Updated Link */}</li>
+                    <li className="nav-item"><Link className="nav-link" to="/app/search">Search</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/app/Login">Login</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/app/register">Register</Link></li>                 
                 </ul>
             </div>
         </nav>
